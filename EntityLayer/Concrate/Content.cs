c135f21dbc774d3de7b9b03324cceeb3bdeb5993
ersinbulut <ersinblt61@gmail.com>
2021-05-11 12:16:50 +0300

    İlişkilerin Oluşturulması 1

--- EntityLayer/Concrate/Content.cs
+++ EntityLayer/Concrate/Content.cs
@@ -13,7 +13,9 @@ namespace EntityLayer.Concrate
         public DateTime ContentDate { get; set; }
         //ContentYazar
         //ContentBaşlık
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
 
-        
+
     }
 }
--- EntityLayer/Concrate/Heading.cs
+++ EntityLayer/Concrate/Heading.cs
@@ -11,5 +11,13 @@ namespace EntityLayer.Concrate
         public int HeadingID { get; set; }
         public string HeadingName { get; set; }
         public DateTime HeadingDate { get; set; }
+
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        public ICollection<Content> Contents { get; set; }
+   
+
     }
 }
