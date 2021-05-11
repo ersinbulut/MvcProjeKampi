703b3b5a4b227c02d2197b3673c30cb078fae86d
ersinbulut <ersinblt61@gmail.com>
2021-05-11 10:03:11 +0300

    Category about contact sınıflarının oluşturulması

---
+++ EntityLayer/Concrate/About.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrate
+{
+   public class About
+    {
+        public int AbautID { get; set; }
+        public string AbautDetails1 { get; set; }
+        public string AbautDetails2 { get; set; }
+        public string AbautImage1 { get; set; }
+        public string AbautImage2 { get; set; }
+
+    }
+}
