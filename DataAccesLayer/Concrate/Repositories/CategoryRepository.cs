269222cf3108d64a59b4a7408cf9cad5ca121ca0
ersinbulut <ersinblt61@gmail.com>
2021-05-30 23:33:16 +0300

    Mvc Proje Kampı 15 Repository Sınıfı Ve Metotların İçinin Doldurulması

---
+++ DataAccesLayer/Concrate/Repositories/CategoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using DataAccesLayer.Abstract;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Concrate.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+        DbSet<Category> _object;
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public void Update(Category p)
+        {
+            c.SaveChanges();
+        }
+
+        /*ToList listeleme
+         Add ekleme
+        Remove Silme
+        Find bulma
+        */
+    }
+}
