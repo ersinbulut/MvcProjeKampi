3052efc7f269e2adfa5c55c30798b8dcc08b3699
ersinbulut <ersinblt61@gmail.com>
2021-05-30 21:52:56 +0300

    Mvc Proje Kampı 8 Key Ve String Length Attributeleri

--- EntityLayer/Concrate/About.cs
+++ EntityLayer/Concrate/About.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace EntityLayer.Concrate
 {
    public class About
     {
+        [Key]
         public int AbautID { get; set; }
+        [StringLength(1000)]
         public string AbautDetails1 { get; set; }
+        [StringLength(1000)]
         public string AbautDetails2 { get; set; }
+        [StringLength(100)]
         public string AbautImage1 { get; set; }
+        [StringLength(100)]
         public string AbautImage2 { get; set; }
 
     }
