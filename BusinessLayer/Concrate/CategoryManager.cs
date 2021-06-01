64d7bba3bb578f687d9927dfd512ec8b66dde3d6
ersinbulut <ersinblt61@gmail.com>
2021-06-01 10:24:21 +0300

    Mvc Proje Kampı 27 Business Abstract & ICategoryService

--- BusinessLayer/Concrate/CategoryManager.cs
+++ BusinessLayer/Concrate/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataAccesLayer.Concrate.Repositories;
+﻿using BusinessLayer.Abstract;
+using DataAccesLayer.Abstract;
+using DataAccesLayer.Concrate.Repositories;
 using EntityLayer.Concrate;
 using System;
 using System.Collections.Generic;
@@ -8,27 +10,33 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrate
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoryService
     {
-        GenericRepository<Category> repo = new GenericRepository<Category>();
 
-        public List<Category> GetAll()
+        ICategoryDal _categoryDal;
+
+        // GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        //public List<Category> GetAll()
+        //{
+        //    return repo.List();
+        //}
+        //public void CategoryAddBL(Category p)
+        //{
+        //    // || veya
+        //    // && ve
+        //    if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)
+        //    {
+        //        //hata mesajı
+        //    }
+        //    else
+        //    {
+        //        repo.Insert(p);
+        //    }
+        //}
+        public List<Category> GetList()
         {
-            return repo.List();
-        }
-        public void CategoryAddBL(Category p)
-        {
-            // || veya
-            // && ve
-            //if (p.CategoryName== ""|| p.CategoryName.Length<=3 || 
-            //    p.CategoryDescription=="" || p.CategoryName.Length>=51)
-            //{
-                //hata mesajı
-            //}
-            //else
-            //{
-                repo.Insert(p);
-            //}
+            return();
         }
     }
 }
--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -18,12 +18,18 @@ namespace MvcProjeKampi.Controllers
         }
         public ActionResult GetCategoryList()
         {
-            var categoryvalues = cm.GetAll();
-            return View(categoryvalues);
+            //var categoryvalues = cm.GetAll();
+            return View();
         }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
         public ActionResult AddCategory(Category p)
         {
-            cm.CategoryAddBL(p);
+            //cm.CategoryAddBL(p);
             return RedirectToAction("GetCategoryList");
         }
     }
