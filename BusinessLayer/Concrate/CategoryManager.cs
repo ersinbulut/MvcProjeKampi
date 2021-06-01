5f67089ebc4a5f4d17f44187b48cd5cd0f9e5dda
ersinbulut <ersinblt61@gmail.com>
2021-06-01 10:34:25 +0300

    Mvc Proje Kampı 28 Constructor Business Metot

--- BusinessLayer/Concrate/CategoryManager.cs
+++ BusinessLayer/Concrate/CategoryManager.cs
@@ -15,28 +15,15 @@ namespace BusinessLayer.Concrate
 
         ICategoryDal _categoryDal;
 
-        // GenericRepository<Category> repo = new GenericRepository<Category>();
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
 
-        //public List<Category> GetAll()
-        //{
-        //    return repo.List();
-        //}
-        //public void CategoryAddBL(Category p)
-        //{
-        //    // || veya
-        //    // && ve
-        //    if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)
-        //    {
-        //        //hata mesajı
-        //    }
-        //    else
-        //    {
-        //        repo.Insert(p);
-        //    }
-        //}
+        
         public List<Category> GetList()
         {
-            return();
+            return _categoryDal.List();
         }
     }
 }
