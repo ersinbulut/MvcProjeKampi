e986b785aaf326327ce8cec3777417dbb58a318d
ersinbulut <ersinblt61@gmail.com>
2021-06-16 15:08:48 +0300

    Mvc Proje Kampı 36 Sidebar İle Beraber Kategori İşlemleri

--- MvcProjeKampi/Controllers/AdminCategoryController.cs
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrate;
+using BusinessLayer.ValidationRules;
 using DataAccesLayer.EntityFramework;
+using EntityLayer.Concrate;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +20,31 @@ namespace MvcProjeKampi.Controllers
             var categoryvalues = cm.GetList();
             return View(categoryvalues);
 
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBL(p);
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result = categoryValidator.Validate(p);
+            if (result.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
